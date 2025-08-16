10d6e3f8a16473de0fc9f4d08e9b3bf16951fac3
Shubh <shubhisroking@proton.me>
2025-08-17 02:04:36 +0530

    add some treesitteer config
    its kinda bad without it
    
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,5 +2,16 @@ return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
   branch = 'main',
-  build = ':TSUpdate'
+  build = ':TSUpdate',
+  opts = {
+        ensure_installed = { 'bash', 'c', 'comment', 'lua', 'vimdoc' },
+        -- auto install is yummy
+        auto_install = true,
+        highlight = {
+            enable = true,
+            -- ruby is kinda bad wihout this
+            additional_vim_regex_highlighting = { 'ruby' },
+        },
+        indent = { enable = true, disable = { 'ruby' } }
+  }
 }
