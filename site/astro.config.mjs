d5e2528b03c667ef921c6955d5fc2fe8c68ea792
Shubh <shubhisroking@proton.me>
2025-08-17 18:47:59 +0530

    change github repo link
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'MaxxVim',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/shubhisroking/maxxvim' }],
 			sidebar: [
 				{
 					label: 'Guides',
