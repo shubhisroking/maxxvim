c230b036f3434f7e18f0451aacf2b218444fb50e
Shubh <shubhisroking@proton.me>
2025-08-18 18:14:16 +0530

    some more nvim plugins
    Signed-off-by: Shubh <shubhisroking@proton.me>

---
+++ lua/plugins/essentials.lua
@@ -0,0 +1,12 @@
+return {
+  { import = "plugins.essentials.telescope" },
+  { import = "plugins.essentials.nvimtree" },
+  { import = "plugins.essentials.cmp" },
+  { import = "plugins.essentials.lsp" },
+  { import = "plugins.essentials.mason" },
+  { import = "plugins.essentials.autopairs" },
+  { import = "plugins.essentials.indentline" },
+  { import = "plugins.essentials.impatient" },
+  { import = "plugins.essentials.comment" },
+  { import = "plugins.essentials.whichkey" },
+}
