2bc01cf160686f08275f4e90a2757eada58f59d9
Shubh <shubhisroking@proton.me>
2025-08-18 18:49:24 +0530

    maxxvim assets page
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,5 +23,6 @@ export default defineConfig({
 				},
 			],
 		}),
+		image(),
 	],
 });
